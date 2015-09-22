402868ebfb95db2f603001b00369271fc39ff8a3
R. S. Doiel <rsdoiel@gmail.com>
2015-09-22 13:35:49 -0700

    design tweaks

--- js/usb.js
+++ js/usb.js
@@ -30,6 +30,83 @@
         searchQueryInput = doc.getElementById("usb-query-input"),
         i = 0;
 
+    var searchWidget = {
+            eds: {
+                filter: [],
+                script: [{src: "http://support.ebscohost.com/eit/scripts/ebscohostsearch.js", type: "text/javascript"}],
+                form: {
+                    method: "GET",
+                    action: "",
+                    onSubmit: "return ebscoHostSearchGo(this);",
+                    input: [
+                        {id: "ebscohostwindow", name: "ebscohostwindow", type: "hidden", value: "0"},
+                        {id: "ebscohosturl", name: "ebscohosturl", type: "hidden", value: "https://clsproxy.library.caltech.edu/login?url=http://search.ebscohost.com/login.aspx?direct=true&site=eds-live&scope=site&type=0&custid=s8984125&groupid=main&profid=eds&mode=bool&lang=en&authtype=ip"},
+                        {id: "ebscohostsearchsrc", name: "ebscohostsearchsrc", type: "hidden", value: "db"},
+                        {id: "ebscohostsearchmode", name: "ebscohostsearchmode", type: "hidden", value: "+"},
+                        {id: "ebscohostkeywords", name: "ebscohostkeywords", type: "hidden", value: ""},
+                        {id: "ebscohostsearchtext", name: "ebscohostsearchtext,", type: "text", size: 23}
+                    ]
+                }
+            },
+            tind: {
+                filter: [
+                    {label: "Title"},
+                    {label: "Author"},
+                    {label: "Subject"},
+                    {label: "Keyword"},
+                    {label: "ISBN"},
+                    {label: "Journal"},
+                    {label: "Abstract"},
+                    {label: "ISSN"}
+                ],
+                form: {
+                    method: "GET",
+                    action: "",
+                    input: [
+                    ]
+                }
+            },
+            sfx: {
+                filter: [],
+                form: {
+                    method: "GET",
+                    action: "",
+                    input: [
+                    ]
+                }
+            },
+            courseReserves: {
+                filter: [
+                    {label: "Course"},
+                    {label: "Instructor"}
+                ],
+                form: {
+                    method: "GET",
+                    action: "",
+                    input: [
+                    ]
+                }
+            },
+            coda: {
+                filter: [],
+                form: {
+                    method: "GET",
+                    action: "",
+                    input: [
+                    ]
+                }
+            },
+            website: {
+                filter: [],
+                form: {
+                    method: "GET",
+                    action: "",
+                    input: [
+                    ]
+                }
+            }
+        };
+
     function addClass(elem, className) {
         var currentClasses = elem.className || "";
         if (currentClasses.indexOf(className) === -1) {
@@ -60,7 +137,7 @@
 
     function menuEventHandler(evt) {
         var elem = evt.target;
-        // we don't use parent of UL here since we're already in a set of nested divs here.
+        // we don't use getParentOfTagName() here since we're already in a set of nested divs here.
         if (elem !== null) {
             switch(elem.tagName.toLowerCase()) {
             case 'a':
@@ -95,19 +172,22 @@
       var cur = getParentOfTagName(elem, "UL"),
           menuSelected = cur.querySelector(".usb-menu-selected"),
           previouslySelected = cur.querySelector(".usb-menu-item-selected");
+
       if (previouslySelected !== null) {
           removeClass(previouslySelected, "usb-menu-item-selected");
       }
       addClass(elem, "usb-menu-item-selected");
       menuSelected.textContent = elem.textContent;
       console.log("DEBUG elem.tagName, elem.textContent? "+elem.tagName+" -> "+elem.textContent);
-      //FIXME: update resource/filter selection.
+      //FIXME: update filter menu from resource selection.
+      //FIXME: update query input form resource selection.
 
       if (cur.id && cur.id === "usb-search-resources") {
-         // Find filters select button.
-         filtersSelectButton.focus();
+          // Do we have a filter list? or do we go straight to query form?
+          // Find filters select button.
+          filtersSelectButton.focus();
       } else {
-        searchQueryInput.focus();
+          searchQueryInput.focus();
       }
       toggleMenu(cur);
     }
@@ -117,6 +197,7 @@
     for (i = 0; i < resourcesMenuItems.length; i++) {
        addMenuItemListener(resourcesMenuItems[i], "click", menuItemEventHandler, false);
     }
+
     filtersSelectButton.addEventListener("click", menuEventHandler, false);
     for (i = 0; i < filtersMenuItems.length; i++) {
        addMenuItemListener(filtersMenuItems[i], "click", menuItemEventHandler, false);
@@ -125,5 +206,4 @@
     /* Add keyboard tab handling to menu */
     resourcesSelectButton.addEventListener("focus", menuEventHandler, false);
     filtersSelectButton.addEventListener("focus", menuEventHandler, false);
-
 }(document, window));
