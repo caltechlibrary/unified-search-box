4015561a46e171b5291dbe01ee5c22417e7f89bd
R. S. Doiel <rsdoiel@gmail.com>
2015-10-13 16:00:59 -0700

    Added single quoting to Tind searches

---
+++ js/tindhostsearch.js
@@ -0,0 +1,29 @@
+/**
+ * tindsearch.js - helper functions for a better tind/website search box integration.
+ */
+/*jslint brower: true */
+(function (window, document) {
+    'use strict';
+
+    function tindQuoteSearch(s) {
+        if (s.indexOf('"') < 0 && s.indexOf("'") < 0) {
+            return "'" + s + "'";
+        }
+        return s;
+    }
+
+    function tindHostSearchGo(form) {
+        var queryInput = document.getElementById('usb-query-input') || null,
+            val = '';
+
+        if (queryInput !== null) {
+            val = queryInput.value;
+        }
+
+        if (val !== '') {
+            val = tindQuoteSearch(val);
+            queryInput.value = val;
+        }
+    }
+    window.tindHostSearchGo = tindHostSearchGo;
+}(window, document));
